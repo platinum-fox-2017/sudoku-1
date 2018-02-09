5ccde0ff0e9d92c32d43214a4af58a9cba63e9c3
Andrew Kusuma <andrew.budikusuma@gmail.com>
2018-02-09 14:22:52 +0700

    Create Board

--- sudoku.js
+++ sudoku.js
@@ -1,12 +1,26 @@
 "use strict"
 
 class Sudoku {
-  constructor(board_string) {}
+  constructor(board_string) { }
 
-  solve() {}
+  solve() { }
 
   // Returns a string representing the current state of the board
-  board() {}
+  board() {
+    let boardSudoku = []
+    let count = 0
+    for (let i = 0; i < 9; i++) {
+      boardSudoku.push([])
+      for (let x = 0; x < 9; x++) {
+        boardSudoku[i].push(board_string[count])
+        count++
+      }
+    }
+    return boardSudoku
+  }
+  // console.log(boardSudoku)
+
+  // return boardSudoku
 }
 
 // The file has newlines at the end of each line,
@@ -19,6 +33,9 @@ var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
 var game = new Sudoku(board_string)
 
 // Remember: this will just fill out what it can and not "guess"
-game.solve()
+// game.solve()
 
 console.log(game.board())
+
+
+
