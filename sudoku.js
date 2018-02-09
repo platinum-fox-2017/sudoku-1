f96a4bf1f172ca13cb92743db5898b373137dd6c
Devdots <devdots@Devdotss-MacBook-Pro.local>
2018-02-09 17:34:59 +0700

    done

--- sudoku.js
+++ sudoku.js
@@ -7,20 +7,52 @@ class Sudoku {
     this.papan = this.board()
     this.check0 = this.check_0()
   }
+  reset_board() {
+    console.log("\x1B[2J")
+  }
 
-
-  solve() {
-    let board = this.papan
-    for (var i = 0; i < this.row; i++) {
-      let row0 = this.check0[i][0]
-      let column0 = this.check0[i][1]
-      for (var j = 1; j <= 9; j++) {
-        if (this.check_column(j, column0) === true && this.check_row(j,row0) === true && this.check_area(j, row0, column0)) {
-          board[row0][column0] = j
-        }
+  sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds) {
+        break;
       }
     }
-    return board
+  }
+
+  solve() {
+    // let this.papan = this.papan
+    for (var i = 0; i < this.check0.length; i++) {
+      let row0 = this.check0[i][0]
+      let column0 = this.check0[i][1]
+      // console.log('col ',column0);
+        if (this.papan[row0][column0] == 0) {
+
+
+        // if ((this.papan[row0][column0]) == 0) {
+
+          for (var j = 1; j <= 9; j++) {
+            if (this.check_column(j, column0) && this.check_row(j,row0) && this.check_area(row0, column0, j)) {
+              this.papan[row0][column0] = j
+
+              this.reset_board()
+              console.log(this.papan)
+              this.sleep(100)
+              let backtrack = this.solve()
+              // console.log('hahahaha ', backtrack);
+              if (backtrack) {
+                return true
+              }
+              else {
+                this.papan[row0][column0] = 0
+              }
+            }
+          }
+          return false
+        // }
+      }
+    }
+    return this.papan
   }
 
   board() {
@@ -36,52 +68,49 @@ class Sudoku {
   }
 
   check_0() {
-    let newBoard = this.papan
-    let check_O = []
+    let check_0 = []
     for (var i = 0; i < this.row; i++) {
       for (var j = 0; j < this.column; j++) {
-        if (newBoard[i][j] === 0) {
-          check_O.push([i,j])
+        if (this.papan[i][j] === 0) {
+          check_0.push([i,j])
         }
       }
     }
-    return check_O
+    return check_0
   }
 
   check_column(number, column) {
-    let newBoard = this.papan
     let newArr = []
     for (var i = 0; i < 9; i++) {
-      newArr.push(newBoard[i][column])
+      newArr.push(this.papan[i][column])
     }
-    return newArr.indexOf(number) === -1
+    return newArr.indexOf(number) == -1
   }
 
   check_row(number, row) {
-    let newBoard = this.papan
-    return newBoard[row].indexOf(number) === -1
+    return this.papan[row].indexOf(number) == -1
   }
 
-  check_area(number, row, column) {
-    let newBoard = this.papan
+  check_area(row, column, number) {
     let columnCorner = 0
     let rowCorner = 0
     let squareSize = 3
     while (row >= rowCorner + squareSize) {
-      rowCorner+= squareSize
+      rowCorner += squareSize
     }
     while (column >= columnCorner + squareSize) {
-      columnCorner+=squareSize
+      columnCorner += squareSize
     }
     for (var i = rowCorner; i < rowCorner+squareSize; i++) {
       for (var j = columnCorner; j < columnCorner+squareSize; j++) {
-        if (newBoard[i][j] === number) {
+        if (this.papan[i][j] === number) {
           return false
         }
       }
     }
     return true
   }
+
 }
 
 // The file has newlines at the end of each line,
