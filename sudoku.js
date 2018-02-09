fa6aa9811b133be95a81f7a17f7cc109ac2c4a9a
Andrew Kusuma <andrew.budikusuma@gmail.com>
2018-02-09 15:16:06 +0700

    checking col and row

--- sudoku.js
+++ sudoku.js
@@ -1,7 +1,9 @@
 "use strict"
 
 class Sudoku {
-  constructor(board_string) { }
+  constructor(board_string) {
+    this.boardsudoku = board_string
+  }
 
   solve() { }
 
@@ -12,15 +14,47 @@ class Sudoku {
     for (let i = 0; i < 9; i++) {
       boardSudoku.push([])
       for (let x = 0; x < 9; x++) {
-        boardSudoku[i].push(board_string[count])
+        boardSudoku[i].push(this.boardsudoku[count])
         count++
       }
     }
     return boardSudoku
   }
-  // console.log(boardSudoku)
 
-  // return boardSudoku
+  ZeroPosition() {
+    let zeroPos = []
+    for (let x = 0; x < this.board().length; x++) {
+      for (let y = 0; y < this.board()[x].length; y++) {
+        if (this.board()[x][y] === '0') {
+          zeroPos.push([x, y])
+        }
+      }
+    }
+    return zeroPos
+  }
+
+  CheckRow(row, values) {
+    for (let x = 0; x < this.board().length; x++) {
+      if (parseInt(this.board()[row][x]) === values) {
+        return false
+      }
+    }
+    return true
+  }
+
+  checkCol(col, values) {
+    for (let y = 0; y < this.board().length; y++) {
+      if (parseInt(this.board()[y][col]) === values) {
+        return false
+      }
+    }
+    return true
+  }
+
+  checkGrid(col, row, values) {
+
+  }
+
 }
 
 // The file has newlines at the end of each line,
@@ -36,6 +70,10 @@ var game = new Sudoku(board_string)
 // game.solve()
 
 console.log(game.board())
+// console.log(game.ZeroPosition())
+console.log(game.CheckRow(0, 9))
+console.log(game.checkCol(0, 7))
+console.log(game.checkGrid(0, 1, 9))
 
 
 
