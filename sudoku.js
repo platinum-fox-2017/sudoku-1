4e1e77f3bd0138091523749d747a36bca323240e
yosaid92 <yohanessahrul92@gmail.com>
2018-02-11 00:10:46 +0700

    release 2

--- sudoku.js
+++ sudoku.js
@@ -1,103 +1,99 @@
-"use strict"
+"use strict";
 
 class Sudoku {
   constructor(board_string) {
-    this.boardString = board_string
-    this.kordinat = 0
-    this.papan = 0;
-    this.multiDimensi = 0;
+    this.board_string = board_string;
+    this.fixedBoard = this.board()
+    this.zeroPosition = this.zeroPosition()
+  }
+
+  board() {
+    let board = [];
+    let inc = 0;
+    for(let i=0; i<9; i++){
+      let row = [];
+      for(let j=0; j<9; j++){
+        row.push(Number(this.board_string[inc]));
+        inc++;
+      }
+      board.push(row);
+    }
+    return board;
+  }
+
+  zeroPosition(){ // simpan kordinat
+    let kordinat = [];
+    for(let i=0; i<this.fixedBoard.length; i++){
+      for(let j=0; j<this.fixedBoard.length; j++){
+        if(this.fixedBoard[i][j] === 0){
+          kordinat.push([i,j]);
+        }
+      }
+    }
+    return kordinat;
+  }
+
+  fillZeroSpace(){
+    let x = 0;
+    let y = 1;
+    let num = 4;
+    
+    if(this.cekRows(x, num) === true && this.cekColumn(y, num) === true){
+      return '['+x+','+y+']' +' Tidak ada angka ' + num;
+    } else {
+      return '['+x+','+y+']' + ' ADA ANGKA ' + num;
+    }
+  }
+
+  cekRows(x, num){
+    for(let i=0; i<this.fixedBoard.length; i++){
+      for(let j=0; j<this.fixedBoard.length; j++){
+        if(i === x){
+          if(this.fixedBoard[i][j] === num){
+            return false;
+          }
+        }
+      }
+    }
+    return true;
+  }
+
+  cekColumn(y, num){
+    for(let i=0; i<this.fixedBoard.length; i++){
+      for(let j=0; j<this.fixedBoard.length; j++){
+        if(j === y){
+          if(this.fixedBoard[i][j] === num){
+            return false;
+          }
+        }
+      }
+    }
+    return true;
+  }
+  cekBlock(x, y, num){
+    
+    
   }
 
   solve() {
-    let x = 0;
-    let y = 1;
-    let angka = '1';
-    if(this.cekBaris(x,angka) === true && this.cekKolom(y,angka) === true ){
-      console.log(angka + ' BISA, Silahkan masukkan angka ke [' + x,y + ']'); // masukkan value baru
-    } else {
-      console.log('Angka sudah ada');
-    }
-    // console.log('-->'+this.cekBaris(0,'7'));
-  }
-
-  // Returns a string representing the current state of the board
-  board() {
-    var stringBoard = this.boardString;
-
-    var inc = 0;
-    var papan = [];
-    var kordinat = [];
-    var multidimensiArray = [];
-    for(let b=0; b<9; b++){
-      var baris = [];
-      var multi = [];
-      for(let c=0; c<9; c++){
-        var detailKordinat = {};
-        if(this.boardString[inc] === '0'){
-          detailKordinat.ind = inc
-          detailKordinat.x = b
-          detailKordinat.y = c
-          detailKordinat.char = this.boardString[inc]
-          multi.push([b, c]);
-          kordinat.push(detailKordinat);
-          baris.push(0);
-        } else {
-          detailKordinat.ind = inc
-          detailKordinat.x = b
-          detailKordinat.y = c
-          detailKordinat.char = this.boardString[inc]
-          // multi.push([b, c, this.boardString[inc]]);
-          kordinat.push(detailKordinat);
-          baris.push(this.boardString[inc]);
-        }
-        inc++;
-      }
-      papan.push(baris.join(' '));
-      multidimensiArray.push(multi);
-    }
-    // console.log(papan);
-    // console.log(kordinat);
-    this.multiDimensi = multidimensiArray;
-    this.papan = papan;
-    this.kordinat = kordinat;
-    return papan;
-  }
-  cekBaris(cekX,angka){
-    for(var b=0; b<this.kordinat.length; b++){
-      if(this.kordinat[b].x === cekX){
-        if(this.kordinat[b].char === angka){
-          return false;
-        }
-      }
-    }
-    return true;
-  }
-  cekKolom(cekY,angka){
-    for(var k=0; k<this.kordinat.length; k++){
-      if(this.kordinat[k].y === cekY){
-        if(this.kordinat[k].char === angka){
-          return false;
-        }
-      } 
-    }
-    return true;
-  }
-  assign(){
     
   }
 }
 
-
 // The file has newlines at the end of each line,
 // so we call split to remove it (\n)
-var fs = require('fs')
-var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
+var fs = require("fs");
+var board_string = fs
+  .readFileSync("set-01_sample.unsolved.txt")
   .toString()
-  .split("\n")[0]
+  .split("\n")[0];
+// console.log(board_string);
 
-var game = new Sudoku(board_string)
+var game = new Sudoku(board_string);
 
 // Remember: this will just fill out what it can and not "guess"
-game.solve()
 
 console.log(game.board())
+console.log(game.fillZeroSpace())
+console.log(game.cekRows())
+console.log(game.solve())
