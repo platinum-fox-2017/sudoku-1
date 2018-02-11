99b2e91aa2818f0c7a8c457100f9c6e99a434e4f
Reza Pramudhika <rezapramudhika@gmail.com>
2018-02-11 18:33:16 +0700

    Akhirnya bisa backtracking

--- sudoku.js
+++ sudoku.js
@@ -4,34 +4,59 @@ class Sudoku {
   constructor(board_string) {
     this.boardString = board_string;
     this.boardArr = this.board();
-    this.fill = [1,2,3,4,5,6,7,8,9];
+    this.emptyBox = this.checkEmptyBox();
+    this.i=0;
+    this.number = 1;
   }
 
-  solve() {
-    var solvedBoard = this.boardArr;
-    for(let i=0; i<solvedBoard.length; i++){
-      for(let j=0; j<solvedBoard[i].length; j++){
-        if(solvedBoard[i][j]==0){
-          for(let k=0; k<this.fill.length; k++){
-            if(this.checker(i, j, this.fill[k].toString(), solvedBoard)){
-              solvedBoard[i][j] = this.fill[k].toString();
-            }
-          }
+  solve(){
+    if(this.i>=this.emptyBox.length){
+      return this.boardArr;
+    }
+
+    if(this.number>9){
+      // console.log('koordinat: ('+this.emptyBox[this.i].i+', '+this.emptyBox[this.i].j+') = '+this.number.toString()+' dan index emptyBox saat ini = '+ this.i);
+      // console.log(this.boardArr)
+      this.i--;
+      this.boardArr[this.emptyBox[this.i].i][this.emptyBox[this.i].j] = '0';
+      this.number = Number(this.emptyBox[this.i].number);
+      this.number++;
+      return this.solve(this.i, this.number);
+    }
+
+    if(this.checker(this.emptyBox[this.i].i, this.emptyBox[this.i].j, this.number.toString())){
+      this.boardArr[this.emptyBox[this.i].i][this.emptyBox[this.i].j] = this.number.toString();
+      this.emptyBox[this.i].number = this.number.toString();
+      //console.log('koordinat: ('+this.emptyBox[this.i].i+', '+this.emptyBox[this.i].j+') = '+this.number.toString()+' dan index emptyBox saat ini = '+ this.i);
+      this.i++;
+      this.number = 1;
+      return this.solve(this.i, this.number)
+    }else{
+      this.number++;
+      return this.solve(this.i, this.number);
+    }
+  }
+
+  checkEmptyBox(){
+    let emptyBox = [];
+    for(let i=0; i<this.boardArr.length; i++){
+      for(let j=0; j<this.boardArr[i].length; j++){
+        if(this.boardArr[i][j]==0){
+          emptyBox.push({i:i, j:j});
         }
       }
     }
-    return solvedBoard;
+    return emptyBox;
   }
 
-  checker(vertical, horizontal, value, board){
+  checker(vertical, horizontal, value){
     for(let i=0; i<9; i++){
-      if(value == board[i][horizontal]){
+      if(value == this.boardArr[i][horizontal]){
         return false;
       }
     }
-
     for(let i=0; i<9; i++){
-      if(value == board[vertical][i]){
+      if(value == this.boardArr[vertical][i]){
         return false;
       }
     }
@@ -39,23 +64,20 @@ class Sudoku {
     let columnCorner = 0;
     let rowCorner = 0;
     let squareSize = 3;
-
     while(horizontal >= columnCorner + squareSize) {
       columnCorner += squareSize;
     }
-
     while(vertical >= rowCorner + squareSize) {
       rowCorner += squareSize;
     }
-
     for(let i=rowCorner; i<rowCorner+squareSize; i++) {
       for(let j=columnCorner; j<columnCorner+squareSize; j++) {
-        if(value == board[i][j]) {        
+        if(value == this.boardArr[i][j]) {        
           return false;
         }
       }
     }
-    
+  
     return true;
   }
 
